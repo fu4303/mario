4c430a058d35e304cf1aa72c54b079bb1e3f0cb9
Mohit Tanwani <mohitmurlidhar@gmail.com>
2020-04-12 00:55:17 +0530

    :tada: Animation Part completed

--- script.js
+++ script.js
@@ -5,8 +5,30 @@ $("#avtar").animate({top: '28vh'},1000,() => {
         $(".name").fadeIn(500);
         setTimeout(()=>{
             $(".name").hide();
-            $(".name").text("I am Mohit Tanwani");
+            $(".name").html("I am<br> Mohit Tanwani");
             $(".name").fadeIn(500);
+            setTimeout(()=>{
+                $(".name").fadeOut(500);
+                var viewportWidth = $(window).width();
+                if(viewportWidth>600)
+                {
+                    $("#avtar").animate({left: '40vw'},2000);
+                    $("#avtar").animate({top: '10.5vh',left:'51.5vw'},500);
+                }
+                else{
+                    $("#avtar").animate({left: '25vw'},1000);
+                    $("#avtar").animate({top: '10.5vh',left:'55vw'},500);
+                }
+                
+                $("#avtar").animate({top: '40vh'},1000,()=>{
+                    $("#avtar").fadeOut(100);
+                    setTimeout(()=>{
+                        $(".header").fadeOut(1000);
+                    },500);
+                    
+                });
+                
+            },2000);
         },1200);
        
     });
