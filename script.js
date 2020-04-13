2d8eb6ffda0c2e15fb439fb537ea225d3351a9a3
Tanwani Mohit <tanwani.mohit@hashedin.com>
2020-04-14 00:20:40 +0530

    Added about me

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-
 animate();
+
 function animate()
 {
+    console.log($("#avtar").css.top);
     $("#avtar").animate({left: '9vw'},2000);
     $("#avtar").animate({top: '38vh'},1000,() => {
         $(".coin").fadeOut(0,() => {
@@ -39,6 +40,9 @@ function animate()
                                     $("body").effect( "shake", {times:2}, 500 );
                                     setTimeout(()=>{
                                         $(".aboutme").hide( "explode", {pieces: 50 }, 2000 );
+                                        setTimeout(()=>{
+                                            typeWriter();
+                                        },2100);
                                         
                                     },500);
                                     
@@ -59,13 +63,19 @@ $( window ).scroll(function() {
      $("#avtar").css({"left":"+={value}"})
 });
 
+var i = 0;
+
+var speed = 50;
+
 function typeWriter() {
-    var i = 0;
-    var txt = 'Hello, I am Mohit Tanwani (if you didnt see it at first)';
-    var speed = 50;
+    var txt = '{// <"name" : "Mohit Tanwani",/<"introduction" : "I am a full stnack developer and I have Master Degree in Information and Technology. I am currently working on MERN stack",/<"email" : "iammohittanwani@gmail.com",/<"Location" : "Ahmedabad, India",/<"Hobbies" : "Photography and Cinematography"  //}';       
     if (i < txt.length) {
-        
-        document.getElementById("myself").innerHTML += txt.charAt(i);
+        if(txt.charAt(i)=='/')
+            document.getElementById("myself").innerHTML += "<br>";
+        else if(txt.charAt(i)=='<')
+            document.getElementById("myself").innerHTML += "&emsp;&emsp;";
+        else
+            document.getElementById("myself").innerHTML += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     }
